ae6466c3bcc97ce8eb40a6b1c6f8ce0cd9a5896b
Javier Zardain <jzardain@fi.uba.ar>
2022-05-22 19:47:38 -0300

    Estado para aeropuerto

--- pkg/model/airport.go
+++ pkg/model/airport.go
@@ -1,7 +1,15 @@
 package model
 
+type AirportStatus string
+
+const (
+	Open   AirportStatus = "OPEN"
+	Closed AirportStatus = "CLOSED"
+)
+
 type Airport struct {
-	Id    int
-	Name  string
-	Place Place
+	Id     int
+	Name   string
+	Place  Place
+	Status AirportStatus
 }
