904e31f2957cd7df2c3e9499320b38f56e0097b0
Javier Zardain <jzardain@fi.uba.ar>
2022-05-25 12:02:52 -0300

    Test flight_service.go Conexion a base de datos en memoria sqlite

--- pkg/controller/flight_controller.go
+++ pkg/controller/flight_controller.go
@@ -1,14 +1,15 @@
 package controller
 
 import (
+	"airport/pkg/database"
 	"airport/pkg/model"
 	"airport/pkg/service"
 )
 
 func GetAllFlights() []model.Flight {
-	return service.GetAllFlights()
+	return service.GetAllFlights(database.GetClient())
 }
 
 func GetAllFlightsByDestination(destination string) []model.Flight {
-	return service.GetAllFlightsByDestination(destination)
+	return service.GetAllFlightsByDestination(database.GetClient(), destination)
 }
--- pkg/database/database_client.go
+++ pkg/database/database_client.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 
 	"gorm.io/driver/mysql"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 var db *gorm.DB
+var inMemoryDB *gorm.DB
 
 func GetClient() *gorm.DB {
 	if db == nil {
@@ -17,10 +19,26 @@ func GetClient() *gorm.DB {
 	return db
 }
 
+func GetInMemoryClient() *gorm.DB {
+	if inMemoryDB == nil {
+		inMemoryDB = connectToInMemoryClient()
+	}
+	return inMemoryDB
+}
+
 func connectToDB() *gorm.DB {
 	var conf = config.GetDBConfig()
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", conf.Username, conf.Password, conf.Host, conf.Port, conf.Database)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+func connectToInMemoryClient() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
