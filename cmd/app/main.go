fb87404b5685658c696972f066f6ebc79cf00fb9
Javier Zardain <jzardain@fi.uba.ar>
2022-06-03 00:15:02 -0300

    Limpieza de estado de los asientos de manera periódica

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"airport/pkg/cron"
 	"airport/pkg/database"
 	"airport/pkg/loader"
 	"airport/pkg/router"
@@ -9,5 +10,10 @@ import (
 func main() {
 	r := router.SetupRouter()
 	loader.LoadTables(database.GetClient())
+	runCronTasks()
 	r.Run(":8080")
+}
+
+func runCronTasks() {
+	go cron.UpdateExpiredReserved()
 }
